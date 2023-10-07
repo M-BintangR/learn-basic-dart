0a166db82da86a9fb7997eb24e7bd6fc874f0910
= <muhbintang650@gmail.com>
2023-10-07 20:20:40 +0800

    operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,20 @@
+/**
+ *  (==  : Sama Dengan)
+ *  (!=  : Tidak Sama Dengan)
+ *  (>   : Lebih Dari)
+ *  (<   : Kurang Dari)
+ *  (>=  : Lebih Besar Atau Sama Dengan)
+ *  (<=  : Lebih Kecil Atau Sama Dengan)
+ */
+
+void main() {
+  print(10 == 10); // true
+  print(10 != 10); // false
+  print(10 > 10); // false
+  print(10 >= 10); // true
+  print(10 < 10); // false
+  print(10 <= 10); // true
+
+  print('Bintang' == 'Bintang'); // true
+  print('Bintang' != 'Bintang'); // false
+}
