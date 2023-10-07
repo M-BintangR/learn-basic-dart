6551c61c0c4c59c0d9ffdfc643a6334cc72938e5
= <muhbintang650@gmail.com>
2023-10-07 19:49:30 +0800

    operator perbandingan

--- bin/operator_aritmatika.dart
+++ bin/operator_aritmatika.dart
@@ -1,1 +1,21 @@
+/**
+ *  (+ => Pertambahan)
+ *  (-, -expresion => Pengurangan atau negatif)
+ *  (* => Perkalian)
+ *  (/ => Pembagian hasil double)
+ *  (~/ => Pembagian hasil int)
+ *  (% => Sisa bagi)
+ * 
+ */
 
+void main() {
+  var first = 10;
+  var second = 3;
+
+  print(first + second);
+  print(first - second);
+  print(first * second);
+  print(first / second);
+  print(first ~/ second);
+  print(first % second);
+}
