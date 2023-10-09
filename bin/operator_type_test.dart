c7fd3cb99f40a08086540db27a65d87944ec20cb
= <muhbintang650@gmail.com>
2023-10-09 11:55:44 +0800

    operator type test

--- bin/operator_type_test.dart
+++ bin/operator_type_test.dart
@@ -1,1 +1,19 @@
-void main() {}
+void main() {
+  dynamic variable = 100;
+
+  // var variableString = variable as string // error;
+
+  var variableInt =
+      variable is int; // cek apakah variable merupakan tipe int atau bukan
+
+  print(variable);
+  print(variableInt);
+
+  print(variable is int);
+  print(variable is bool);
+  print(variable is String);
+
+  print(variable is! int); // apakah variable bukan tipe int
+  print(variable is! bool);
+  print(variable is! String);
+}
