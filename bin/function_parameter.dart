bac56f9109d09032488f359e032c9f3e9bbae9ef
= <muhbintang650@gmail.com>
2023-10-09 13:35:26 +0800

    function parameter

---
+++ bin/function_parameter.dart
@@ -0,0 +1,9 @@
+void sayHello(String firstName, String lastName) {
+  print('Hello $firstName $lastName');
+}
+
+void main() {
+  sayHello('Muhammad', 'Bintang');
+  sayHello('Fery', 'Fadul');
+  sayHello('Syahru', 'Ramadhan');
+}
